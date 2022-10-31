aaa655ae9ebb230aefd7e17f30149cbea2196bd9
Akiyamov <116005851+Akiyamov@users.noreply.github.com>
2022-11-01 00:06:01 +0500

    переезд

--- catalog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import ilaksa
-
-# Register your models here.
-
-@admin.register(ilaksa)
-class ilaksaAdmin(admin.ModelAdmin):
-    list_display = ('name', 'descr', 'date_start', 'status_idea', 'date_finish')
-    list_filter = ('status_idea',)
--- catalog/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import re_path
-
-urlpatterns = [
-    re_path(r'^$', views.index, name='index'),
-    re_path(r'^live_idea/$', views.LiveIdeaListView.as_view(), name='living_idea'),
-    re_path(r'^dead_idea/$', views.DeadIdeaListView.as_view(), name='dead_idea'),
-    re_path(r'^new_idea/$', views.ilaksa_new, name='new_idea'),
-]
